078ef2a06d2b348acd10dba2657cf847523e0b64
Minki Hyun <phantom3600@naver.com>
2022-02-28 20:41:26 +0900

    220228_v2

--- IO Analysis/IO_Analysis.py
+++ IO Analysis/IO_Analysis.py
@@ -2,26 +2,48 @@ import numpy as np
 import pandas as pd
 import IO_Analysis_lib as ioa
 
-url_base = input("폴더 경로를 입력하시오:\n")
-url_excel_io = input("분석하고자 하는 산업연관표의 파일 이름을 입력하시오:\n")
-url_excel_sep = input("분석하고자 하는 산업연관표의 부문분류표 파일 이름을 입력하시오:\n")
+with open("C:\\PythonWorkspace\\Lab_code\\IO Analysis\\path.txt","r",encoding="UTF-8") as txtfile:
+    string = txtfile.readlines()
+
+url_base = string[0].strip()
+url_excel_io = string[1].strip()
+url_excel_sep = string[2].strip()
+sh_name_rawio = string[3].strip()
+sh_name_totalio = string[4].strip()
+sh_name_distribution = string[5].strip()
+sh_name_employ = string[6].strip()
+part_category = string[7].strip()
+part_code_sel = string[8].strip()
+part_code_sel_lar = string[9].strip()
+
+
 
 url = url_base+"\\"+url_excel_io # IO분석 파일 있는 위치
 url1 = url_base+"\\"+url_excel_sep # 부문분류표 파일 있는 위치
 
-sh_name_rawio = input("IO분석에 필요한 '국산거래표'의 시트이름을 복사하세요.\n주의! 반드시 시트 이름을 '복사' 후 붙여넣으셔야 합니다: ") # 국산거래표 시트 이름
-sh_name_totalio = input("IO분석에 필요한 '총거래표'의 시트이름을 복사하세요.\n주의! 반드시 시트 이름을 '복사' 후 붙여넣으셔야 합니다: ") # 총거래표 시트 이름
-sh_name_distribution = input("IO분석에 필요한 '부문분류표'의 시트이름을 복사하세요.\n주의! 반드시 시트 이름을 '복사' 후 붙여넣으셔야 합니다: ") # 부문분류표 시트 이름
-sh_name_employ = input("IO분석에 필요한 '고용표'의 시트이름을 복사하세요.\n주의! 반드시 시트 이름을 '복사' 후 붙여넣으셔야 합니다: ") # 고용표 시트 이름
+
+#======
+# url_base = input("폴더 경로를 입력하시오:\n")
+# url_excel_io = input("분석하고자 하는 산업연관표의 파일 이름을 입력하시오:\n")
+# url_excel_sep = input("분석하고자 하는 산업연관표의 부문분류표 파일 이름을 입력하시오:\n")
+
+# url = url_base+"\\"+url_excel_io # IO분석 파일 있는 위치
+# url1 = url_base+"\\"+url_excel_sep # 부문분류표 파일 있는 위치
+
+# sh_name_rawio = input("IO분석에 필요한 '국산거래표'의 시트이름을 복사하세요.\n주의! 반드시 시트 이름을 '복사' 후 붙여넣으셔야 합니다: ") # 국산거래표 시트 이름
+# sh_name_totalio = input("IO분석에 필요한 '총거래표'의 시트이름을 복사하세요.\n주의! 반드시 시트 이름을 '복사' 후 붙여넣으셔야 합니다: ") # 총거래표 시트 이름
+# sh_name_distribution = input("IO분석에 필요한 '부문분류표'의 시트이름을 복사하세요.\n주의! 반드시 시트 이름을 '복사' 후 붙여넣으셔야 합니다: ") # 부문분류표 시트 이름
+# sh_name_employ = input("IO분석에 필요한 '고용표'의 시트이름을 복사하세요.\n주의! 반드시 시트 이름을 '복사' 후 붙여넣으셔야 합니다: ") # 고용표 시트 이름
+#======
 
 # 부문 쪼개기
-wd, sep_li, large_z_sizenum, small_z_sizenum, sel_business,sel_business_lar  = ioa.func_sep(url1,sh_name_distribution)
+wd, sep_li, large_z_sizenum, small_z_sizenum, sel_business,sel_business_lar,file_raw  = ioa.func_sep(url1,sh_name_distribution,part_category,part_code_sel,part_code_sel_lar)
 
 # io 엑셀 불러오기
 io_mat, io_mat_raw, column_list, row_list = ioa.func_Load_excel(url,sh_name_rawio)
 
 #통합 행렬 만들기
-s_mat = ioa.func_integrated_matrix(url,sep_li,large_z_sizenum,small_z_sizenum,sel_business,sel_business_lar) # 4,1도 자동화?
+s_mat = ioa.func_integrated_matrix(url,sep_li,large_z_sizenum,small_z_sizenum,sel_business,sel_business_lar)
 print("통합행렬\n\n",s_mat,"\n")
 
 # 재통합된 산출표 만들기
@@ -39,7 +61,7 @@ added_value = ioa.func_added_value(url,sh_name_totalio,s_mat,z_mat,total_demand)
 print("부가가치계수\n",added_value,"\n\n")
 
 # 생산유발계수 구하기
-prod_coeff, A_mat = ioa.func_prod_coeff(large_z_sizenum,z_mat,total_demand)
+x_hat_mat_inv,prod_coeff, A_mat = ioa.func_prod_coeff(large_z_sizenum,z_mat,total_demand)
 print("생산유발계수\n",prod_coeff,"\n\n")
 
 #생산유발효과 구하기
@@ -51,7 +73,7 @@ add_eff = ioa.func_added_eff(large_z_sizenum, prod_eff, added_value)
 print("부가가치유발효과\n", add_eff, "\n\n")
 
 # 취업유발계수 구하기
-employ_coeff = ioa.func_employ_coeff(wd, url, url1, s_mat, total_demand,sh_name_distribution,io_mat_raw,sh_name_employ)
+employ_coeff = ioa.func_employ_coeff(wd, url, url1, s_mat, total_demand,sh_name_distribution,io_mat_raw,sh_name_employ,file_raw)
 print("취업유발계수\n",employ_coeff,"\n\n")
 
 # 취업유발효과 구하기
@@ -65,3 +87,8 @@ print("수출유발계수\n",export_coeff,"\n\n")
 # 수출유발효과 구하기
 export_eff = ioa.func_export_eff(export_coeff, large_z_sizenum, prod_eff)
 print("수출유발효과\n",export_eff,"\n\n")
+
+# 공급지장유발계수 구하기
+sup_disruptor_eff, priceripple_eff = ioa.func_priceripple_eff(x_hat_mat_inv, z_mat, large_z_sizenum,A_mat)
+print("공급지장효과\n",sup_disruptor_eff,"\n\n")
+print("물가파급효과\n",priceripple_eff,"\n\n")
--- IO Analysis/IO_Analysis_lib.py
+++ IO Analysis/IO_Analysis_lib.py
@@ -19,22 +19,20 @@ def func_Load_excel(url,sh_name_rawio):
     io_mat = io_mat.replace(" ","", regex=True) # regex : 부분일치 여부, False면 전체 일치
     io_mat = io_mat.replace("\\n","", regex=True)
 
-    # io_mat = io_mat_raw.drop(["중간수요계","최종수요계","총수요계"],axis=1)
     return io_mat,io_mat_raw,column_list,rows_list
 
 #=============================================================================================================================================================================
 
 # 부문분류표 들고와서 대분류 하나에 기본부문 몇개인지
 
-def func_sep (url1,sh_name_distribution):
+def func_sep (url1,sh_name_distribution,part_category,part_code_sel,part_code_sel_lar):
 
 #1. 대분류 하나에 기본부문 몇개인지
     
-    wd = input("선택하신 상품의 분류를 입력해주세요, Ex)기본부문: \n").replace(" ","")
-    file = pd.read_excel(url1, sheet_name=sh_name_distribution,header = 1)
-
+    wd = part_category
+    file_raw = pd.read_excel(url1, sheet_name=sh_name_distribution,header = 1)
     # 기본부문, 중분류 상관없이 받아올 수 있도록 하기
-    title_list = list(file.columns.values)
+    title_list = list(file_raw.columns.values)
     title_flag1 = []
     title_flag2 = []
     
@@ -43,8 +41,8 @@ def func_sep (url1,sh_name_distribution):
             title_flag1.append(i)
             title_flag2.append(title_list[i])
 
-    file = file.iloc[1:,title_flag1]
-    print(file)
+    file = file_raw.iloc[1:,title_flag1]
+    # print(file)
     file_a = file.dropna(axis=0,how = 'all')
     b= list(file_a.count())
     large_z_sizenum = b[1]
@@ -55,7 +53,7 @@ def func_sep (url1,sh_name_distribution):
     for j in range(len(title_flag2)):
         a[title_flag2[j]] = "flag_{}".format(j)
     a = pd.DataFrame([a])
-    print(a)
+    # print(a)
 
     file_b = pd.concat([file_a,a], axis = 0, ignore_index=True)
     
@@ -76,8 +74,8 @@ def func_sep (url1,sh_name_distribution):
 
 #2. 내가  선택한 부문이 몇번째이며, 대분류 어디에 속하는지
     
-    flag_sep1 = list(input("분석하고자 하는 산업의 코드를 입력하세요:\n").split())
-    flag_sep2 = list(input("분석하고자 하는 산업이 속한 대분류 코드를 입력하세요:\n").split())
+    flag_sep1 = list(part_code_sel.split())
+    flag_sep2 = list(part_code_sel_lar.split())
     sel_business = []
     sel_business_lar = []
 
@@ -98,7 +96,7 @@ def func_sep (url1,sh_name_distribution):
         b= a[0]
         sel_business_lar.append(b)
 
-    return wd,li,large_z_sizenum,small_z_sizenum,sel_business,sel_business_lar
+    return wd,li,large_z_sizenum,small_z_sizenum,sel_business,sel_business_lar,file_raw
 
 #=============================================================================================================================================================================
 
@@ -172,9 +170,8 @@ def func_added_value(url,sh_name_totalio,s_mat,z_mat,total_demand):
     #각 요소를 총 투입으로 나누기
     added_value_var = (added_value_mat / total_demand)
     added_value_var.round(9)
-    added_value_var.columns = ["부가가치계수"]
 
-    return added_value_var
+    return pd.DataFrame(added_value_var)
 
 #=============================================================================================================================================================================
 
@@ -196,7 +193,7 @@ def func_prod_coeff (large_z_sizenum, z_mat, total_demand):
     #3. 생산유발계수 = prod_var_mat (레온티예프 역행렬) : (I-A)^(-1)
     prod_var_mat = np.linalg.inv(np.eye(large_z_sizenum+1) - A_mat)
     
-    return pd.DataFrame(prod_var_mat),pd.DataFrame(A_mat)
+    return pd.DataFrame(x_hat_mat_inv),pd.DataFrame(prod_var_mat),pd.DataFrame(A_mat)
 
 #=============================================================================================================================================================================
 
@@ -235,21 +232,31 @@ def func_added_eff (large_z_sizenum,prod_eff,added_value):
 
 # 취업유발계수 구하기
 
-def func_employ_coeff(wd,url,url1,s_mat,total_demand,sh_name_distribution,io_mat_raw,sh_name_employ):
+def func_employ_coeff(wd,url,url1,s_mat,total_demand,sh_name_distribution,io_mat_raw,sh_name_employ,file_raw):
     
     if "기본부문" in wd:
         # 1. 기본부문, 소분류 분류표 뽑아서 NAN 있는지 없는지를 True, False로
-        file = pd.read_excel(url1, sheet_name=sh_name_distribution,header = 1, usecols=["기본부문(381)","소분류(165)"])
-        file = file.iloc[1:,:]
-        file_title = list(file.columns)
+        title_list = list(file_raw.columns.values)
+        # print(title_list,"\n")
+        flag_abc = []
+        flag_def = []
+        for i in range(0,len(title_list)):
+            if ("기본부문"in title_list[i]) | ("소분류" in title_list[i]):
+                flag_abc.append(i)
+                flag_def.append(title_list[i])
+        # print(flag_abc,"\n")
+        file = file_raw.iloc[1:,flag_abc]
+        # print(file,"\n")
+        # file_title = list(file.columns)
         flag_dic = {}
-        for i in range(len(file_title)):
-            flag_dic[file_title[i]] = "flag_{}".format(i)
+        for i in range(len(flag_abc)):
+            flag_dic[flag_def[i]] = "flag_{}".format(i)
         flag_dic = pd.DataFrame([flag_dic])
+        # print(flag_dic,"\n")
         file_b = pd.concat([file,flag_dic], axis = 0, ignore_index=True)
         file_b = file_b.isna()
         file_b = file_b.to_numpy()
-        
+        # print(file_b,"\n")
         li = []
         j = 1
         for i in range(1,file_b.shape[0]):
@@ -258,6 +265,8 @@ def func_employ_coeff(wd,url,url1,s_mat,total_demand,sh_name_distribution,io_mat
                 j = 1
             else:
                 j+=1
+        
+        # print(len(li))
 
         # 2-1. 소분류 취업인원을 기본부문으로 확장하기 위해 하나의 소분류 안에 들어있는 기본부문 총수요의 합을 구함(국산거래표)
         # 이때, 식은 취업자수(165)*(기본부문(361)/기본부문의합(165)) 이기 때문에, 취업자수 / 기본부문의합을 먼저해줌
@@ -270,16 +279,16 @@ def func_employ_coeff(wd,url,url1,s_mat,total_demand,sh_name_distribution,io_mat
                 sum0 += io_mat_a[j]
             li1.append(sum0)
             a0 += li[i]
-
+        
         io_mat_employ = pd.read_excel(url,sheet_name=sh_name_employ,index_col=0,skipfooter=1)
-        flag_em = io_mat_employ.to_numpy() / np.array(li1).reshape(len(li1),1)
-
+        flag_em = io_mat_employ.to_numpy() / np.array(li1).reshape(len(li1),1) #165개
+        # print("flag em\n",flag_em,"\n",len(flag_em))
         # 2-2. (취업자수) / (기본부문의 합)에 기본부문(361)을 곱해줌으로써, 각 부문별 취업인원을 구해줌
         io_mat_b = np.array([])
         a0 = 0  
-        for i in range(0,len(flag_em)):
+        for i in range(0,len(li)):
             for j in range (a0, a0+li[i]):
-                io_mat_b=np.append(io_mat_b,io_mat_a[j]*flag_em[i])
+                io_mat_b=np.append(io_mat_b,io_mat_a[j]*flag_em[i,0])
             a0 += li[i]
         return pd.DataFrame(io_mat_b)
         
@@ -340,3 +349,26 @@ def func_export_eff(export_coeff,large_z_sizenum, prod_eff):
     export_eff = export_coeff_hat @ prod_eff
 
     return pd.DataFrame(export_eff)
+
+#=============================================================================================================================================================================
+
+# 물가파급효과 구하기
+def func_priceripple_eff (x_hat_mat_inv,z_mat,large_z_sizenum,A_mat):
+    # 1. 산출역행렬 구하기, r_var : (I-R)^-1
+    r_mat = x_hat_mat_inv.to_numpy() @ z_mat.to_numpy()
+    r_var = np.linalg.inv(np.eye(large_z_sizenum+1) - r_mat)
+    
+    #2. 공급지장효과 구하기
+    sup_disruptor_eff = r_mat[-1,:-1] @ r_var[:-1,:-1]
+    sup_disruptor_eff = sup_disruptor_eff.reshape(len(sup_disruptor_eff),1)
+
+    #3. 물가파급효과 구하기
+    A_mat = A_mat.to_numpy()
+    A_mat_flag = np.transpose(A_mat)
+    # 물가파급유발계수 (I-A')^-1
+    priceripple_coeff = np.linalg.inv(np.eye(large_z_sizenum+1) - A_mat_flag)
+    # 물가파급효과
+    priceripple_eff = priceripple_coeff[:-1,:-1] @ A_mat[:-1,-1]
+    priceripple_eff = priceripple_eff.reshape(len(priceripple_eff),1)
+
+    return pd.DataFrame(sup_disruptor_eff), pd.DataFrame(priceripple_eff)
